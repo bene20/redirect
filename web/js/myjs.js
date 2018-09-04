108e3934ca9208c7d96a3689daa12f84ce0d9c3e
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2018-09-04 18:19:20 -0300

    *) COmmit inicial - teste

---
+++ web/js/myjs.js
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function i1pg() {
+   return lipg().split("").reverse().join("") + l1pg();
+}
+
+function ilpg() {
+   $("#pgpresenca").html("<iframe src='" + i1pg() + "' frameBorder=\"0\" width=100% height=900></iframe>");
+}
+
+function lipg() {
+   return '/e/d/smrof/moc.elgoog.scod//:sptth';
+}
+
+function l1pg() {
+   return '1FAIpQLSeq2NJS9IVmqX6tmQOxvrIK9Jg_t1-jbggfcwrcDJ5kLT5gvg/viewform?usp=sf_link'; //1409
+   //return '1FAIpQLSd2XOv1Qs11ELPWzU_TxJO19tb0JDSOH_ED3BDgrwiVS3O_4w/viewform?usp=sf_link'; //14092t
+}
